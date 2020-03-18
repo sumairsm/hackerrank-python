a5f2bee84f800481dc8fe8f02cde7ba38e20e49d
Sumair <sumairsm@gmail.com>
2020-03-17 23:36:03 -0400

    hackerrank python challenge

--- minionGame.py
+++ minionGame.py
@@ -2,9 +2,39 @@ def minion_game(string):
      splitString = s.split("").lower
      for i in splitString:
          if i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u':
-             for j in splitString
+             for j in   splitString
 
 
 if __name__ == '__main__':
     s = input()
     minion_game(s)
+
+
+
+
+
+
+""""
+Kevin and Stuart want to play the 'The Minion Game'.
+
+Game Rules
+
+Both players are given the same string,
+.
+Both players have to make substrings using the letters of the string
+
+.
+Stuart has to make words starting with consonants.
+Kevin has to make words starting with vowels.
+The game ends when both players have made all possible substrings.
+
+Scoring
+A player gets +1 point for each occurrence of the substring in the string
+
+.
+
+For Example:
+String
+= BANANA
+Kevin's vowel beginning word = ANA
+Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points. """
